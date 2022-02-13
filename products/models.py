039a10281518e0f21003f5e0497b885b212eed93
tR1nZ <lesnikovvova111@yandex.ru>
2022-02-13 16:42:26 +0300

    сделал модели и выполнил миграции

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
+from distutils.command.upload import upload
+from email.mime import image
+from pydoc import describe
+from statistics import quantiles
+from unicodedata import category
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=512)
+    image = models.ImageField(upload_to='products_images', blank=True, null=True)
+    description = models.CharField(max_length=512)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
