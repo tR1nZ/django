a61051afb93e01b344462c29c388f2aac5f6712f
tR1nZ <lesnikovvova111@yandex.ru>
2022-02-13 18:24:49 +0300

    добавил рабочую медиа

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from sys import unraisablehook
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from products.views import index, products
 
@@ -23,3 +26,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('products/', products, name='products'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
